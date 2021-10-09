f34c929a4978e9540fecce68f98f80b2c6e8657a
Jasmine Meyer <45775227+jmeyer27@users.noreply.github.com>
2021-10-09 20:13:22 +0000

    Changed Comments once again

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 //CS 433 Assignment 3
 //Your names: Jasmine Meyer and Birhane Diarra
 //File type: main.cpp
-//Purpose: Creates Shell 
+//Purpose: When the program receives a command it puts it in a buffer and parses it, the parser turns the buffer into token arguments. The arguments are then checked to see if they are either a shell command or a user command by using an “if-else statement”. When it is a user command it gets passed to executeUserCommand, and if it is a shell command it gets passed to executeShellCommand. When the argument is a shell command, a child process gets made by forking the parent, the arguments are then passed into a function that executes the program.
 //===========================================================
 #include <iostream> //project is page 204 of textbook
 #include <stdio.h>
@@ -10,6 +10,9 @@
 #include <string.h>
 #include "Shell.h"
 
+
+
+//The main method is the entry point into the program.
 int main(){
 cout << "CS 433 Programming assignment 2\n"
 	"Author: Birhane Diarra and Jasmine Meyer\n"
@@ -23,13 +26,14 @@ cout << "CS 433 Programming assignment 2\n"
   //while shell is running, keep asking for commands to run processes
   while (osh.isRunning) {
 		char buffer[MAX];					// array to hold chars from command line to be interpreted
-		char history_buffer[MAX];				// used to save the command for history feature
+		char history_buffer[MAX];				// used to save the commands for history feature
 		char * args[MAX / 2 + 1];			// arguments of the command
 
 		std::cout << "osh> ";
 		std::cout.flush();//keeps line clear for user input
 
-		std::cin.getline(buffer, MAX);		// reads command from input.		r, buffer);			// copy the buffer
+		std::cin.getline(buffer, MAX);		// reads command from user input
+    strcpy(history_buffer, buffer);			// copy the buffer to history_buffer
 
     //shell parses buffer for arguments
 		osh.parseline(buffer, args);
